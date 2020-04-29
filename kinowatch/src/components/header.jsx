e6e15a3716584e2e226869c456d40c43ca611382
corbie1 <bratchikovjohn@gmail.com>
2020-04-29 13:20:22 +0300

    www

---
+++ kinowatch/src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import  Logo from '../images/KinoWatch.png'
+import  Night from '../images/night.png'
+
+
+function Header (props){
+    return(
+        <header className = "header__app header">
+            <a href="#" className="header__logo">
+                <img src={Logo} />
+            </a>
+            <ul className = "header__menu">
+                <li className = "menu__item"><a href = "#">Home</a></li>
+                <li className = "menu__item">New</li>
+                <li className = "menu__item">Movies</li>
+                <li className = "menu__item">Anime</li>
+                <li className = "menu__item">My list</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
