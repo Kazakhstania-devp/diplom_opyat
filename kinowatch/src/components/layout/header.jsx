5a611214a8ec0e5496426df11fe755d4b849a041
corbie1 <bratchikovjohn@gmail.com>
2020-04-30 23:31:33 +0300

    Update header.jsx

--- kinowatch/src/components/layout/header.jsx
+++ kinowatch/src/components/layout/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  BrowserRouter as Router ,Switch, Route, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import  Logo from '../../images/KinoWatch.png';
 import Search from '../searchBar/search.jsx';
 import  '../route/route.jsx';
