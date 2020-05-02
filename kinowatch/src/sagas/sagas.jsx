ac80e36b9d005b208a8c46afaec24a22754cad53
corbie1 <bratchikovjohn@gmail.com>
2020-05-02 17:02:58 +0300

    1

--- kinowatch/src/sagas/sagas.jsx
+++ kinowatch/src/sagas/sagas.jsx
@@ -1,35 +1,20 @@
 import { call, put, all, takeLatest, select } from 'redux-saga/effects';
 
-import {Operation} from '../api/operations';
+import {Operation} from '../src/api/operations';
 import {ActionType} from '../store/action-types';
-import {getPrevTrack, getNextTrack} from '../store/selectors';
-import {ActionCreator} from '../store/actions';
 
 function* fetchInitialData() {
   try {
     const [profile, music] = yield all([call(Operation.fetchProfile), call(Operation.fetchMusic)]);
 
     yield all([
-      put(ActionCreator.setMyProfile(profile.data)),
-      put(ActionCreator.setMyTracks(music.data.tracks)),
-      put(ActionCreator.setCurrentTrack(music.data.tracks[0])),
-      put(ActionCreator.setCurrentTracklist(music.data.tracks)),
-      put(ActionCreator.setMyPlaylists(music.data.playlists)),
-      put(ActionCreator.setIsLoaded(true)),
+     
     ]);
 
   } catch (e) {}
 }
 
-function* setPrevTrackAsCurrent() {
-  const prevTrack = yield select(getPrevTrack);
-  yield put(ActionCreator.setCurrentTrack(prevTrack));
-}
 
-function* setNextTrackAsCurrent() {
-  const nextTrack = yield select(getNextTrack);
-  yield put(ActionCreator.setCurrentTrack(nextTrack));
-}
 
 
 export default function* sagaWatcher() {
