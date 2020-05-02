a6df0d55a6961bc038900e73e03e8cddfbaef0e2
corbie1 <bratchikovjohn@gmail.com>
2020-05-02 16:55:11 +0300

    Update adapters.jsx

--- kinowatch/api/adapters.jsx
+++ kinowatch/api/adapters.jsx
@@ -1,8 +1,5 @@
 import {Profile} from '../types'; // eslint-disable-line
 
 export const profileAdapter = (data) => ({
-  id: data.id,
-  firstName: data.first_name,
-  lastName: data.last_name,
-  avatarUrl: data.avatarUrl
+
 });
