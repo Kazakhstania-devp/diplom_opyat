a528d58ca99d5dc6fff77bacf56a29a2a5d12304
Andrew <46646012+Rayness@users.noreply.github.com>
2020-05-02 20:29:06 +0300

    Update operations.jsx

--- kinowatch/src/api/operations.jsx
+++ kinowatch/src/api/operations.jsx
@@ -1,5 +1,6 @@
 import {ApiEndpoint} from '../config/api-endpoints';
 import {ActionCreator} from '../store/actions';
+import history from '../history';
 import {createAPI} from '../api/api';
 
 const api = createAPI(() => history.push(RouteConfig.INDEX));
