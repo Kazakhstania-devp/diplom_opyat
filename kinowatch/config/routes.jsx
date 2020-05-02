5bba669a1a77605a851d23993141161cec6605b3
corbie1 <bratchikovjohn@gmail.com>
2020-05-02 16:47:26 +0300

    Create routes.jsx

---
+++ kinowatch/config/routes.jsx
@@ -0,0 +1,13 @@
+const Route {
+    INDEX = '/',
+  }
+  
+  export default Route;
+  
+  export const isPrivateRoute = (pathname) => {
+    const myListRe = /\/mylist/;
+    const addReviewRe = /\/film\/[1-9][\d]*\/review/;
+  
+    return myListRe.test(pathname) || addReviewRe.test(pathname);
+  };
+  
