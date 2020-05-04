d30b6e32304cba8140889ed99aa5d28adf5e2c4a
corbie1 <bratchikovjohn@gmail.com>
2020-05-04 18:39:03 +0300

    Update Slider.jsx

--- kinowatch/src/components/Slider/Slider.jsx
+++ kinowatch/src/components/Slider/Slider.jsx
@@ -5,36 +5,48 @@ var settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToShow: 6,
+    slidesToScroll: 2
   };
 
 function Slick (){
     return(
         <Slider {...settings}>
         <div>
-      
         <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
         </div>
         <div>
         <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
-
         </div>
         <div>
         <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
-
         </div>
         <div>
         <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
-
         </div>
         <div>
         <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
-
         </div>
         <div>
         <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
-
+        </div>
+        <div>
+        <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
+        </div>
+        <div>
+        <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
+        </div>
+        <div>
+        <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
+        </div>
+        <div>
+        <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
+        </div>
+        <div>
+        <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
+        </div>
+        <div>
+        <img src="https://via.placeholder.com/150/000000/FFFFFF/?text=IPaddress.net"></img>
         </div>
       </Slider>
     )
