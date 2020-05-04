aa9297b32e246b3456ad364ff9717f20cfd720b0
Andrew <46646012+Rayness@users.noreply.github.com>
2020-05-04 21:37:04 +0300

    OwO

--- kinowatch/src/components/layout/header.jsx
+++ kinowatch/src/components/layout/header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import  Logo from '../../images/KinoWatch.png';
-import Search from '../searchBar/search';
+import Search from '../searchBar/Search';
 import  '../../routes';
 
 function Header (props){
