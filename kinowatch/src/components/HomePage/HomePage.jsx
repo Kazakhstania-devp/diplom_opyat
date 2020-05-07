a773c1dbf765035c9391b053365690edb0f4c6c9
corbie1 <bratchikovjohn@gmail.com>
2020-05-07 22:45:42 +0300

    Update HomePage.jsx

--- kinowatch/src/components/HomePage/HomePage.jsx
+++ kinowatch/src/components/HomePage/HomePage.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import Slick from '../Slider/Slider'
 function HomePage(){
     return (
+        <div>
         <Slick name="Popular"/>
+        <Slick name="Anime"/>
+        </div>
     )
 }
 export default HomePage;
