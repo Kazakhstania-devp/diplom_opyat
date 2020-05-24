c5c6f02b96dc02e19431e0a5854a580e122011c7
corbie1 <bratchikovjohn@gmail.com>
2020-05-24 22:25:32 +0300

    Update HomePage.jsx

--- kinowatch/src/components/HomePage/HomePage.jsx
+++ kinowatch/src/components/HomePage/HomePage.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import Slick from '../Slider/Slider'
 import {useSelector} from 'react-redux';
+import {useParams} from "react-router-dom";
+import { createSelector } from "reselect";
+
+
+
+export const pageByRubricList = createSelector(
+    (state) => state.rubricList,
+    (rubricList) => (rubricName) => (id) => (
+    ( rubricList.length !==0 && rubricList[rubricName].list.find((movie) => movie.id == id)) || null
+    ),
+);
+
+
 
 function HomePage(){
+
+    //Poster
+    const poster = useSelector((state) => pageByRubricList(state));
+
+
+    //Slider
     const rubric = useSelector((state) => state.rubricList);
+
+
     return (
         <div>
-        <div className="main-poster">
+                <div className="main-poster">
                 <div className="image-poster"></div>
-            </div>
+        </div>
         <div>
             {Object.keys(rubric).map((key) => (
             <Slick name = {rubric[key].name} genre = {rubric[key].list}/>
