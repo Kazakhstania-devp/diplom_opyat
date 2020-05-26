a437bcc674dcbc0f0bda0cdfb55dd1bccbeb92af
corbie1 <bratchikovjohn@gmail.com>
2020-05-26 20:10:38 +0300

    Main page poster

--- kinowatch/src/components/HomePage/HomePage.jsx
+++ kinowatch/src/components/HomePage/HomePage.jsx
@@ -6,36 +6,57 @@ import { createSelector } from "reselect";
 
 
 
-export const pageByRubricList = createSelector(
+
+export const randomMovie = createSelector(
     (state) => state.rubricList,
     (rubricList) => (rubricName) => (id) => (
     ( rubricList.length !==0 && rubricList[rubricName].list.find((movie) => movie.id == id)) || null
     ),
 );
 
-
-
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function HomePage(){
 
+
     //Poster
-    const poster = useSelector((state) => pageByRubricList(state));
+    var rndFilm = 5;
+    var name = "movies";
+    const poster = useSelector((state) => state.rubricList[name]);
+    if(poster != null){
+        rndFilm = getRandomInt(0, poster.list.length);
+    }
 
 
+    const rndPoster = useSelector((state) => randomMovie(state)(name)(rndFilm));
+  
     //Slider
     const rubric = useSelector((state) => state.rubricList);
 
 
-    return (
+
+    if (rndPoster != null) {
+        for(var key in rndPoster){
+        return (
+            
         <div>
-                <div className="main-poster">
-                <div className="image-poster"></div>
-        </div>
-        <div>
+            <div className="main-poster">
+                <img src={rndPoster.poster} className="image-poster"></img>
+            </div>
+            <div>
             {Object.keys(rubric).map((key) => (
-            <Slick name = {rubric[key].name} genre = {rubric[key].list}/>
+                <Slick name = {rubric[key].name} genre = {rubric[key].list}/>
             ))}
+            </div>
         </div>
-        </div>
-    )
-}
+        );
+        }
+      }
+      return (
+        <div>404</div>
+      );
+    }
 export default HomePage;
