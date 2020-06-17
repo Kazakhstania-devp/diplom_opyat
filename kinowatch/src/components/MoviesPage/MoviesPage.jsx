3517346f64e78760737e58b1e1fd8d1df6123752
Andrew <46646012+Rayness@users.noreply.github.com>
2020-06-17 14:57:55 +0300

    1

--- kinowatch/src/components/MoviesPage/MoviesPage.jsx
+++ kinowatch/src/components/MoviesPage/MoviesPage.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import Slick from '../Slider/Slider'
+import {useSelector} from 'react-redux';
+import {useParams} from "react-router-dom";
+import { createSelector } from "reselect";
+import {Poster} from "../Poster/Poster";
 
-function MoviesPage(){
-    return <h1>Movies</h1>
-}
-export default MoviesPage;
+function HomePage(){
+  
+    //Селектор для получения фильмов в слайдер
+    const rubric = useSelector((state) => state.rubricList);
+
+    //
+    return(
+        <div>
+            <Poster name="movies"></Poster>
+             <div>
+            {Object.keys(rubric).map((key) => (
+                <Slick name = {rubric[key].name} genre = {rubric[key].list}/>
+            ))}
+            </div>
+        </div>
+    )
+    
+    
+    }
+export default HomePage;
--- kinowatch/src/components/NewPage/NewPage.jsx
+++ kinowatch/src/components/NewPage/NewPage.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import Slick from '../Slider/Slider'
+import {useSelector} from 'react-redux';
+import {useParams} from "react-router-dom";
+import { createSelector } from "reselect";
+import {Poster} from "../Poster/Poster";
 
-function NewPage(){
-    return <h1>New</h1>
-}
-export default NewPage;
+function HomePage(){
+  
+    //Селектор для получения фильмов в слайдер
+    const rubric = useSelector((state) => state.rubricList);
+
+    //
+    return(
+        <div>
+            <Poster name="new"></Poster>
+             <div>
+            {Object.keys(rubric).map((key) => (
+                <Slick name = {rubric[key].name} genre = {rubric[key].list}/>
+            ))}
+            </div>
+        </div>
+    )
+    
+    
+    }
+export default HomePage;
